0c4bc23936519c557d91a721b9b3f4090a986eb2
Eyuel <eyuelmathewos@gmail.com>
2020-11-23 23:05:21 +0300

    upload to remote repo

--- src/index.js
+++ src/index.js
@@ -9,10 +9,9 @@ import {BrowserRouter } from 'react-router-dom';
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import  modalReducer from './redux/reducers/modal'
-import movielistReducer from './redux/reducers/movieReducer';
 import { Provider } from 'react-redux';
 const middleware = [thunk];
-const allReducers = combineReducers({movies: movielistReducer, modal: modalReducer})
+const allReducers = combineReducers({ modal: modalReducer })
 const store = createStore(allReducers, compose( applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 store.subscribe(()=>{
--- src/redux/Action/addMovie.js
+++
@@ -1,7 +0,0 @@
-export const UPDATEMOVIES = "UPDATEMOVIES";
-const updateMovies = (movies)=>({
-    type: 'UPDATEMOVIES',
-    payload: movies
-});
-
-export default updateMovies;
